7b9db074f37adaf87e85b4284fcbec7576c61bdf
Myo Thura Zaw <mtrzmyothurazaw@gmail.com>
2023-03-05 08:08:34 +0630

    Complete the project

--- NullMeBaby/ViewController.m
+++ NullMeBaby/ViewController.m
@@ -13,10 +13,44 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+// nonnull, _Nonnull & _Nullable
+-(int)sum:(nonnull NSNumber*)numA :(nonnull NSNumber*)numB {
+    int sum = numA.intValue + numB.intValue;
+    return sum;
 }
 
+-(void)updateName:(NSString* _Nonnull)name {
+    
+}
+
+-(void)updateName2:(NSString* _Nullable)name {
+    
+}
+
+-(void)idiot:(NSString* _Nonnull)grr {
+    
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+//    NSString *str;
+//    [self getName:str];
+    [self updateName:nil];          // only show warning
+    [self updateName2:nil];
+    
+    NSNumber *num1;
+    NSNumber *num2;
+    
+    int sum = [self sum:num1 :num2];
+    
+//    if (num1 && num2) {
+//        int sum = [self sum:num1 :num2];
+//        NSLog(@"Sum: %d",sum);
+//    } else {
+//        NSLog(@"These are null!");
+//    }
+        
+}
 
 @end
